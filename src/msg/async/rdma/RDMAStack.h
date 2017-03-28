892169ad53729045a1c761c7ac89727de7fde461
Haomai Wang <haomai@xsky.com>
2017-03-28 17:03:40 -0500

    msg/async/rdma: perf record zerocopy chunk and bytes
    Signed-off-by: Haomai Wang <haomai@xsky.com>

--- src/msg/async/rdma/RDMAStack.h
+++ src/msg/async/rdma/RDMAStack.h
@@ -144,6 +144,9 @@ enum {
 
   l_msgr_rdma_tx_chunks,
   l_msgr_rdma_tx_bytes,
+  l_msgr_rdma_tx_zerocopy_chunks,
+  l_msgr_rdma_tx_zerocopy_bytes,
+
   l_msgr_rdma_rx_chunks,
   l_msgr_rdma_rx_bytes,
 
